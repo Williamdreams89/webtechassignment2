e0ea08138e1100651dc5e329df1c2d798963ef3d
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-03-14 13:19:57 +0000

    Calculation functionalities done

--- apply.js
+++ apply.js
@@ -1,10 +1,38 @@
 const form = document.getElementById("reg-form");
 
+const programmeFeeCharges = {
+  healthInformation: 5000,
+  mathematics: 3500,
+  medicine: 7000,
+  internationalBusiness: 2500,
+  politicalScience: 2000,
+};
 
+const hallCharges = {
+  src: 400,
+  valco: 300,
+  adehye: 300,
+  ssnit: 350,
+  casford: 300,
+};
+
+function calculateTotalFees(
+  selectedProgramme,
+  selectedHallOfResidence,
+  selectedCafeteriaOption
+) {
+  const programmeFee = programmeFeeCharges[selectedProgramme];
+  const hallCharge = hallCharges[selectedHallOfResidence];
+  const cafeteriaCharge = selectedCafeteriaOption === "Yes" ? 120 : 0;
+
+  console.log(`Hall charges - ${hallCharge}`);
+  return programmeFee + hallCharge + cafeteriaCharge;
+}
 
 function validateForm() {
   const selectedGender = new FormData(form).get("gender");
-  const selectedHallOfResidence = document.getElementById("hallsOfresidence").value;
+  const selectedHallOfResidence =
+    document.getElementById("hallsOfresidence").value;
   const dateOfBirth = new Date(document.getElementById("date-of-birth").value);
 
   // 1. if a male gender selects adehye hall in the select option, alert "Not allowed"
@@ -42,38 +70,91 @@ function validateForm() {
   }
 
   return true;
-
 }
-
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  // Invoke DOB validation here 
+  // Invoke DOB validation here
 
   const isValid = validateForm();
 
-  if(isValid){
-    // Get form values 
+  if (isValid) {
+    // Get form values
 
-  const firstName = document.getElementById("first-name").value;
-  const lastName = document.getElementById("last-name").value;
-  const selectedProgramme = document.getElementById("programme").value;
-  const aggregate = document.getElementById("aggregate").value;
-  const formerSchool = document.getElementById("former-school").value;
-  const selectedGender = new FormData(event.target).get("gender");
-  const dateOfBirth = document.getElementById("date-of-birth").value;
-  const selectedHallOfResidence =
-    document.getElementById("hallsOfresidence").value;
-  const selectedCaferiaOption = new FormData(event.target).get("cafeteria");
+    const firstName = document.getElementById("first-name").value;
+    const lastName = document.getElementById("last-name").value;
+    const selectedProgramme = document.getElementById("programme").value;
+    const aggregate = document.getElementById("aggregate").value;
+    const formerSchool = document.getElementById("former-school").value;
+    const selectedGender = new FormData(event.target).get("gender");
+    const dateOfBirth = document.getElementById("date-of-birth").value;
+    const selectedHallOfResidence =
+      document.getElementById("hallsOfresidence").value;
+    const selectedCaferiaOption = new FormData(event.target).get("cafeteria");
 
-  // Formatted date
+    // Calculate total fees
+    // Calculating the total fee charge
+    let hallFee = 0;
+    let programFee = 0;
+    let cafeteriaFee = 0;
 
-  const formattedDOB = new Date(dateOfBirth)
-  const formattedDOBOutput = formattedDOB.toLocaleDateString("en-US",  { month: 'long', day: 'numeric', year: 'numeric' })
-  console.log(`formatted date = ${formattedDOBOutput}`)
-  // Redirect to table.html with form data as URL parameters
-  const url = `prepareBill.html?firstName=${firstName}&lastName=${lastName}&selectedProgramme=${selectedProgramme}&aggregate=${aggregate}&formerSchool=${formerSchool}&selectedGender=${selectedGender}&dateOfBirth=${formattedDOBOutput}&selectedHallOfResidence=${selectedHallOfResidence}&selectedCaferiaOption=${selectedCaferiaOption}`;
-  window.location.href = url;
+    switch (selectedHallOfResidence) {
+      case "A":
+        hallFee = 5000;
+        break;
+      case "B":
+        hallFee = 6000;
+        break;
+      case "C":
+        hallFee = 7000;
+        break;
+    }
+
+    switch (selectedProgramme) {
+      case "BSc(Health Information)":
+        programFee = 10000;
+        break;
+      case "BSc(Mathematics)":
+        programFee = 12000;
+        break;
+      case "BSc(Medicine)":
+        programFee = 8000;
+        break;
+      case "BSc(Political science)":
+        programFee = 8000;
+        break;
+      case "BSc(International Business)":
+        programFee = 8000;
+        break;
+    }
+
+    switch (selectedCaferiaOption) {
+      case "yes":
+        cafeteriaFee = 1500;
+        break;
+      case "no":
+        cafeteriaFee = 0;
+        break;
+    }
+
+    const totalFees = hallFee + programFee + cafeteriaFee;
+
+    console.log(totalFees);
+
+    // Formatted date
+
+    const formattedDOB = new Date(dateOfBirth);
+    const formattedDOBOutput = formattedDOB.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+    console.log(`formatted date = ${formattedDOBOutput}`);
+    // Redirect to table.html with form data as URL parameters
+    const url = `prepareBill.html?firstName=${firstName}&lastName=${lastName}&selectedProgramme=${selectedProgramme}&aggregate=${aggregate}&formerSchool=${formerSchool}&selectedGender=${selectedGender}&dateOfBirth=${formattedDOBOutput}&selectedHallOfResidence=${selectedHallOfResidence}&selectedCaferiaOption=${selectedCaferiaOption}&totalFeesCharged=${totalFees}`;
+    window.location.href = url;
+
+    console.log(`total fees = ${totalFees}`);
   }
 });
--- prepareBill.js
+++ prepareBill.js
@@ -1,18 +1,18 @@
-// Get data from urlparams 
-const urlParams = new URLSearchParams(window.location.search)
-const first_name = urlParams.get("firstName")
-const last_name = urlParams.get("lastName")
-const selectedProgramme = urlParams.get("selectedProgramme")
-const aggregate = urlParams.get("aggregate")
-const formerSchool = urlParams.get("formerSchool")
-const selectedGender = urlParams.get("selectedGender")
-const dateOfBirth = urlParams.get("dateOfBirth")
-const selectedHallOfResidence = urlParams.get("selectedHallOfResidence")
-const selectedCaferiaOption = urlParams.get("selectedCaferiaOption")
-
+// Get data from urlparams
+const urlParams = new URLSearchParams(window.location.search);
+const first_name = urlParams.get("firstName");
+const last_name = urlParams.get("lastName");
+const selectedProgramme = urlParams.get("selectedProgramme");
+const aggregate = urlParams.get("aggregate");
+const formerSchool = urlParams.get("formerSchool");
+const selectedGender = urlParams.get("selectedGender");
+const dateOfBirth = urlParams.get("dateOfBirth");
+const selectedHallOfResidence = urlParams.get("selectedHallOfResidence");
+const selectedCaferiaOption = urlParams.get("selectedCaferiaOption");
+const totalFeeCharged = urlParams.get("totalFeesCharged")
 
 // Create table row with URL parameter values
-const row = document.createElement('tr');
+const row = document.createElement("tr");
 row.innerHTML = `
 	<td>${first_name}</td>
 	<td>${last_name}</td>
@@ -23,15 +23,13 @@ row.innerHTML = `
 	<td>${dateOfBirth}</td>
 	<td>${selectedHallOfResidence}</td>
 	<td>${selectedCaferiaOption}</td>
+	<td>${totalFeeCharged}</td>
 	
 `;
 
+
+
 // Get the table body element and append the new row
-const tableBody = document.getElementById('table_body');
+const tableBody = document.getElementById("table_body");
 tableBody.appendChild(row);
 
-
-
-console.log(`First name is ${first_name}`)
-console.log(`First name is ${last_name}`)
-console.log(`First name is ${selectedProgramme}`)
